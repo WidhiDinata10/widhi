9ee3c0c152d5936f5094ef55a9bc47b266dcd55e
WidhiDinata10 <79655256+WidhiDinata10@users.noreply.github.com>
2021-03-07 20:11:09 +0700

    Add files via upload

---
+++ widhi/index.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <div class="jumbotron" id="jumbotron">
+        <div class="container">
+            <div class="text-center">
+                <img src="asset/profile.jpeg" width="300" class="rounded-circle img-thumbnail">
+                <h1 class="display-4">BIODATA DIRI</h1>
+                <p class="lead">Biodata Diri Saya</p>
+            </div>
+        </div>
+    </div>
+
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Widhi Dinata</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="#">Home<span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link" href="#">about</a>
+                <a class="nav-item nav-link" href="#">Biodata Diri </a>
+                <a class="nav-item nav-link" href="media/form.html">Registers</a>
+            </div>
+        </div>
+    </nav>
+
+    <!--about-->
+    <section class="about" id="about">
+        <div class="container">
+            <div class="row mb-4">
+                <div class="col text-center">
+                    <h2>about</h2>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-md-5">Ayah saya bernama Made Widhiada bekerja sebagai anggota POLRI yang berasal dari sangsit, yang memiliki seoarang 3 anak yang bernama Widhi Adnyani, Widhi Dinata, Widhi Dharma Pratiwi.
+                    <p>.</p>
+                </div>
+                <div class="col-md-5">
+                    <p>Ibu saya bernama Ketut Sujani bekerja sebagai Ibu Rumah Tangga yang berasal dari sangsit, yang memiliki seoarang 3 anak yang bernama Widhi Adnyani, Widhi Dinata, Widhi Dharma Pratiwi.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--portofololio-->
+    <section class="portfolio" id="portfolio">
+        <div class="container">
+            <div class="row pt-5 mb-4">
+                <div class="col text-center">
+                    <h2>Pengalaman</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md mb-4">
+                    <div class="card">
+                        <img class="card-img-top" src="asset/profile 2.jpeg" class="card-img-top">
+                         <div class="card-body">
+                            <p class="card-text">Saya memiliki pengalaman dalam mengikuti tes kedinasan salah satunya, tes kedinasan IPDN namun saya sudah 2 kali gagal dalam mengikutinya.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md mb-4">
+                    <div class="card">
+                        <img class="card-img-top" src="asset/profile 3.jpeg" class="card-img-top">
+                         <div class="card-body">
+                            <p class="card-text">Saya memiliki penglaman dalam berorganisasi karena saya ingin menambah teman dan menambah wawasan dalam berlingkungan dikampus</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+</html>
